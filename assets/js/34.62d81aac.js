3aef34c8c7d1982e8b6116a2faa7064fe8a73481
liugang <lgcf1314@gmail.com>
2021-12-19 16:51:04 +0800

    [ADD]修改左下侧音频组件

--- assets/js/34.62d81aac.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[34],{733:function(t,e,n){"use strict";n.r(e);var s=n(4),o=Object(s.a)({},(function(){var t=this.$createElement,e=this._self._c||t;return e("ContentSlotsDistributor",{attrs:{"slot-key":this.$parent.slotKey}},[e("Boxx",{attrs:{changeTime:"5000"}})],1)}),[],!1,null,null,null);e.default=o.exports}}]);
